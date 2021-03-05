427f14ebe9a1f01104addcaeaa663ba3e9846c06
AlexandraZaharova <zakharova911@gmail.com>
2021-03-05 20:05:23 +0300

    feat: implement function min

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        array.sort( (a, b) => a - b );
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
